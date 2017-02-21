e4c2202a190b8cfdeed442374cc2d39d598f47cf
Loic Tauveron <loic.tauveron62@gmail.com>
2017-02-21 15:23:45 +0100

    create gallerie_controler

---
+++ app/controllers/galleries_controller.rb
@@ -0,0 +1,26 @@
+class GalleriesController < ApplicationController
+  before_action :set_gallery, only: [:show]
+
+  # GET /galleries
+  # GET /galleries.json
+  def index
+    @galleries = Gallery.all
+  end
+
+  # GET /galleries/1
+  # GET /galleries/1.json
+  def show
+  end
+
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_gallery
+      @gallery = Gallery.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def gallery_params
+      params.require(:gallery).permit(:name, :published)
+    end
+end
